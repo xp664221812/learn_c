52603e8bbc69edb207e8d58b3779f49fba446b9c
xp664221812 <realxp141414@163.com>
2018-08-01 21:03:08 +0800

    add sth

--- create_list/List.c
+++ create_list/List.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #define LIST_INIT_SIZE 100
 #define LIST_INCREAMENT 10
 
@@ -19,8 +21,27 @@ typedef struct SqList
 } SqList, *Ptr;
 typedef Ptr SqListPtr;
 
-Status List_init(SqListPtr l)
+//初始化线性表
+enum Status List_init(SqListPtr L)
 {
-    Status s = success;
+    enum Status s = success;
+    L->list_size = LIST_INIT_SIZE;
+    L->elem = (ElemType *)malloc(sizeof(ElemType) * L->list_size);
+    if (L->elem == NULL)
+    {
+        s = fatal;
+    }
+    return s;
 }
+
+
+//按线性表位置查找
+enum Status List_Retrival(SqListPtr L,int pos,ElemType *elem)
+{
+    enum Status s=range_error;
+    
+
+    return s;
+}
+
 
